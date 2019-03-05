22bbb69734d2e5a6f85ff7a14ce9f10ff12d78dc
denizsen97 <denizsen97@gmail.com>
2019-03-05 20:03:20 +0300

    Added LoginActivity and SignupActivity using the template activities

--- app/src/main/java/com/bookclub/app/bookclub/WelcomeActivity.java
+++ app/src/main/java/com/bookclub/app/bookclub/WelcomeActivity.java
@@ -1,9 +1,16 @@
 package com.bookclub.app.bookclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button loginButton, signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,26 @@ public class WelcomeActivity extends AppCompatActivity {
         System.out.println("Hi");
         System.out.println("sadsdasfsd");
         System.out.println("sanksfksdösdmaksfkjasfkjsd");
+        loginButton = findViewById(R.id.loginButton);
+        signInButton = findViewById(R.id.signInButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(WelcomeActivity.this, "Login Test", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                intent.putExtra("User", 873638);
+                startActivity(intent);
+
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(WelcomeActivity.this, "Signin Test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
