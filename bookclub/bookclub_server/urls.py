361c487ae575bb84bd94796f0c29c8c9e8d463c9
mahinkhankishizade <mahinkhankishizade@gmail.com>
2019-03-16 03:21:33 +0300

    usercontroller finished and checked
    everything works fine (hash needs to be added later, forgot password will send mails)

---
+++ bookclub/bookclub_server/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import usercontroller
+urlpatterns = [
+    path('login/', usercontroller.login),
+    path('signup/', usercontroller.signup),
+    path('forgotPassword/', usercontroller.forgot_password),
+    path('signout/', usercontroller.sign_out),
+    path('getSession/', usercontroller.get_session),
+    path('confirmMatch/', usercontroller.confirm_match),
+    path('rejectMatch/', usercontroller.reject_match),
+    path('seeOtherUserProfile/', usercontroller.see_other_user_profile),
+    path('matchListIndex/', usercontroller.match_list_index),
+    path('suggestionListIndex/', usercontroller.suggestion_list_index),
+    path('mainMenuIndex/', usercontroller.main_menu_index),
+    path('searchIndex/', usercontroller.search_index)
+]
